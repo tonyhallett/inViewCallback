dfe0f4d8cbd1b04dc16c2886961d87d2e05bddde
Tony Hallett <tonyhallett74@gmail.com>
2018-06-12 17:10:45 +0100

    minification

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var UglifyJS = require('uglify-js');
+var fs=require('fs');
+var path=require('path');
+
+exports.default=function(done){
+    fs.readFile(__dirname + path.sep + "inViewCallback.js","utf8",function(err,data){
+        if(err){
+            done(err);
+        }else{
+            var minified=UglifyJS.minify(data);
+            if(minified.error){
+                done(err);
+            }else{
+                fs.writeFile(__dirname + path.sep + "inViewCallback.min.js",minified.code,function(err){
+                    done(err);
+                });
+            }            
+        }
+    });
+};
